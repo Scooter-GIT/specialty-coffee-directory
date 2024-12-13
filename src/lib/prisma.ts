b156a5d41b057c9cefdef2cecfed1b60d50a0e49
Scooter-GIT <54643838+Scooter-GIT@users.noreply.github.com>
2024-12-13 17:09:38 -0600

    Update error handling and configurations

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,9 +4,12 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma ||
-  new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  });
+const prismaClient = new PrismaClient({
+  log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+});
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+export const prisma = global.prisma || prismaClient;
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
